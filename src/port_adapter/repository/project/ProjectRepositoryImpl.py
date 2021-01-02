e66b9d0f959f2588de254201e480d7eae4e147b9
Arkan M. Gerges <arkan.m.gerges@gmail.com>
2021-01-02 20:21:24 +0200

    Add db port

--- src/port_adapter/repository/project/ProjectRepositoryImpl.py
+++ src/port_adapter/repository/project/ProjectRepositoryImpl.py
@@ -22,7 +22,7 @@ class ProjectRepositoryImpl(ProjectRepository):
     def __init__(self):
         try:
             self._db = create_engine(
-                f"mysql+mysqlconnector://{os.getenv('CAFM_PROJECT_DB_USER', 'root')}:{os.getenv('CAFM_PROJECT_DB_PASSWORD', '1234')}@{os.getenv('CAFM_PROJECT_DB_HOST', '127.0.0.1')}/{os.getenv('CAFM_PROJECT_DB_NAME', 'cafm-project')}")
+                f"mysql+mysqlconnector://{os.getenv('CAFM_PROJECT_DB_USER', 'root')}:{os.getenv('CAFM_PROJECT_DB_PASSWORD', '1234')}@{os.getenv('CAFM_PROJECT_DB_HOST', '127.0.0.1')}:{os.getenv('CAFM_PROJECT_DB_PORT', '3306')}/{os.getenv('CAFM_PROJECT_DB_NAME', 'cafm-project')}")
             SessionFactory = sessionmaker(bind=self._db)
             self._dbSession: Session = SessionFactory()
         except Exception as e:
