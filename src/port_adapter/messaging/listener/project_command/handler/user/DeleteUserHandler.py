e6cb2a1e66439d925be84a36fa4923a50d821cd9
Arkan M. Gerges <arkan.m.gerges@gmail.com>
2021-01-07 09:09:34 +0200

    Delete user when user is deleted from the identity

---
+++ src/port_adapter/messaging/listener/project_command/handler/user/DeleteUserHandler.py
@@ -0,0 +1,41 @@
+"""
+@author: Arkan M. Gerges<arkan.m.gerges@gmail.com>
+"""
+import json
+import time
+
+import src.port_adapter.AppDi as AppDi
+from src.application.UserApplicationService import UserApplicationService
+from src.domain_model.resource.exception.UnAuthorizedException import UnAuthorizedException
+from src.domain_model.user.User import User
+from src.port_adapter.messaging.listener.CommandConstant import CommonCommandConstant
+from src.port_adapter.messaging.listener.project_command.handler.Handler import Handler
+from src.resource.logging.logger import logger
+
+
+class DeleteUserHandler(Handler):
+
+    def __init__(self):
+        self._commandConstant = CommonCommandConstant.CREATE_USER
+
+    def canHandle(self, name: str) -> bool:
+        return name == self._commandConstant.value
+
+    def handleCommand(self, messageData: dict) -> dict:
+        name = messageData['name']
+        data = messageData['data']
+        metadata = messageData['metadata']
+
+        logger.debug(
+            f'[{DeleteUserHandler.handleCommand.__qualname__}] - received args:\ntype(name): {type(name)}, name: {name}\ntype(data): {type(data)}, data: {data}\ntype(metadata): {type(metadata)}, metadata: {metadata}')
+        appService: UserApplicationService = AppDi.instance.get(UserApplicationService)
+        dataDict = json.loads(data)
+        metadataDict = json.loads(metadata)
+
+        if 'token' not in metadataDict:
+            raise UnAuthorizedException()
+
+        obj: User = appService.deleteUser(id=dataDict['id'], token=metadataDict['token'])
+        return {'name': self._commandConstant.value, 'created_on': round(time.time() * 1000),
+                'data': {'id': obj.id()},
+                'metadata': metadataDict}
