385e1719e316d11d37349640dc551a84f212ee6f
Mohammad Mohammadi <mmdii@develoop.run>
2021-02-24 10:16:15 +0330

    equipment category bug fixed

--- src/port_adapter/repository/db_model/EquipmentCategory.py
+++ src/port_adapter/repository/db_model/EquipmentCategory.py
@@ -4,6 +4,7 @@
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import DateTime
 
 import src.port_adapter.AppDi as AppDi
@@ -18,6 +19,10 @@ class EquipmentCategory(Base):
     createdAt = Column('created_at', DateTime, nullable=True, default=datetime.utcnow())
     modifiedAt = Column('modified_at', DateTime, nullable=True, onupdate=datetime.utcnow())
 
+    # Relationship
+    groups = relationship(
+        "EquipmentCategoryGroup",
+        back_populates="category", lazy='joined')
 
     def __repr__(self):
         return f"[Repo DB Model] EquipmentCategory(id='{self.id}', name='{self.name}')"
