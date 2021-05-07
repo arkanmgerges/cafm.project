2bb9cc9f782f6606e8259a1de6c0214d05949ccb
alexbrebu <alex.brebu13@gmail.com>
2021-05-07 19:35:32 +0300

    feat: create migration to add new columns to subcontractor table

---
+++ src/resource/db_migration/versions/0040__add_columns_to_subcontractor.py
@@ -0,0 +1,60 @@
+from sqlalchemy import *
+from migrate import *
+
+meta = MetaData()
+
+tbl = Table("subcontractor", meta)
+
+col0 = Column("description", String(255), nullable=True)
+col1 = Column(
+    "country_id",
+    Integer,
+    ForeignKey(
+        "country.geoname_id",
+        name="fk__subcontractor__country_id",
+        ondelete="CASCADE",
+        onupdate="CASCADE",
+    ),
+    nullable=True,
+)
+col2 = Column(
+    "city_id",
+    Integer,
+    ForeignKey(
+        "city.geoname_id",
+        name="fk__subcontractor__city_id",
+        ondelete="CASCADE",
+        onupdate="CASCADE",
+    ),
+    nullable=True,
+)
+col3 = Column("postal_code", String(30), nullable=True)
+
+
+def upgrade(migrate_engine):
+    meta.bind = migrate_engine
+    Table("country", meta, autoload=True)
+    Table("city", meta, autoload=True)
+    col0.create(tbl)
+    col1.create(tbl)
+    col2.create(tbl)
+    col3.create(tbl)
+
+
+def downgrade(migrate_engine):
+    meta.bind = migrate_engine
+    with migrate_engine.connect() as conn:
+        conn.execute("ALTER TABLE subcontractor DROP COLUMN description")
+
+        conn.execute(
+            "ALTER TABLE subcontractor DROP CONSTRAINT fk__subcontractor__country_id"
+        )
+        conn.execute("ALTER TABLE subcontractor DROP COLUMN country_id")
+
+        conn.execute(
+            "ALTER TABLE subcontractor DROP CONSTRAINT fk__subcontractor__city_id"
+        )
+        conn.execute("ALTER TABLE subcontractor DROP COLUMN city_id")
+
+        conn.execute("ALTER TABLE subcontractor DROP COLUMN postal_code")
+        pass
