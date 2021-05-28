27052e0baeafcb9ac1e363002b2038f45db36a6d
Arkan M. Gerges <arkan.m.gerges@gmail.com>
2021-05-28 14:34:30 +0300

    lookup - equipment wip

---
+++ src/port_adapter/messaging/listener/common/handler/lookup/equipment/CreateEquipmentHandler.py
@@ -0,0 +1,45 @@
+"""
+@author: Arkan M. Gerges<arkan.m.gerges@gmail.com>
+"""
+import json
+
+import src.port_adapter.AppDi as AppDi
+from src.application.lookup.equipment.EquipmentLookupApplicationService import EquipmentLookupApplicationService
+from src.domain_model.resource.exception.UnAuthorizedException import (
+    UnAuthorizedException,
+)
+from src.port_adapter.messaging.listener.CommandConstant import CommonCommandConstant
+from src.port_adapter.messaging.listener.common.handler.Handler import Handler
+from src.resource.common.Util import Util
+from src.resource.logging.logger import logger
+
+
+class CreateEquipmentHandler(Handler):
+    def __init__(self):
+        self._commandConstant = CommonCommandConstant.CREATE_EQUIPMENT
+
+    def canHandle(self, name: str) -> bool:
+        return name == self._commandConstant.value
+
+    def handleCommand(self, messageData: dict, extraData: dict = None) -> dict:
+        name = messageData["name"]
+        data = messageData["data"]
+        metadata = messageData["metadata"]
+
+        logger.debug(
+            f"[{CreateEquipmentHandler.handleCommand.__qualname__}] - received args:\ntype(name): {type(name)}, name: {name}\ntype(data): {type(data)}, data: {data}\ntype(metadata): {type(metadata)}, metadata: {metadata}"
+        )
+        appService: EquipmentLookupApplicationService = AppDi.instance.get(EquipmentLookupApplicationService)
+        dataDict = json.loads(data)
+        metadataDict = json.loads(metadata)
+
+        if "token" not in metadataDict:
+            raise UnAuthorizedException()
+
+        dataDict["id"] = dataDict.pop("equipment_id")
+        appService.createEquipment(
+            **Util.snakeCaseToLowerCameCaseDict(dataDict),
+            token=metadataDict["token"],
+        )
+
+        return None
---
+++ src/port_adapter/messaging/listener/project_command/handler/lookup/equipment/CreateEquipmentHandler.py
@@ -0,0 +1,10 @@
+"""
+@author: Mohammad S. moso<moso@develoop.run>
+"""
+
+from src.port_adapter.messaging.listener.common.handler.lookup.equipment.CreateEquipmentHandler import (
+    CreateEquipmentHandler as Handler,
+)
+
+class CreateEquipmentHandler(Handler):
+    pass
