aa0cab1fca34451a2472aadbbf0266a8038861b8
alexbrebu <alex.brebu13@gmail.com>
2021-06-24 07:50:08 +0300

    refactor: lint the files

--- src/port_adapter/api/grpc/listener/lookup/EquipmentLookupAppServiceListener.py
+++ src/port_adapter/api/grpc/listener/lookup/EquipmentLookupAppServiceListener.py
@@ -7,41 +7,70 @@ import src.port_adapter.AppDi as AppDi
 from src.application.lookup.equipment.Building import Building
 from src.application.lookup.equipment.BuildingLevel import BuildingLevel
 from src.application.lookup.equipment.BuildingLevelRoom import BuildingLevelRoom
-from src.application.lookup.equipment.EquipmentCategoryGroup import EquipmentCategoryGroup
-from src.application.lookup.equipment.EquipmentLookupApplicationService import EquipmentLookupApplicationService
+from src.application.lookup.equipment.EquipmentCategoryGroup import (
+    EquipmentCategoryGroup,
+)
+from src.application.lookup.equipment.EquipmentLookupApplicationService import (
+    EquipmentLookupApplicationService,
+)
 from src.application.lookup.equipment.EquipmentModel import EquipmentModel
-from src.application.lookup.equipment.EquipmentProjectCategory import EquipmentProjectCategory
+from src.application.lookup.equipment.EquipmentProjectCategory import (
+    EquipmentProjectCategory,
+)
 from src.application.lookup.equipment.MaintenanceProcedure import MaintenanceProcedure
-from src.application.lookup.equipment.MaintenanceProcedureOperation import MaintenanceProcedureOperation
-from src.application.lookup.equipment.MaintenanceProcedureOperationParameter import \
-    MaintenanceProcedureOperationParameter
+from src.application.lookup.equipment.MaintenanceProcedureOperation import (
+    MaintenanceProcedureOperation,
+)
+from src.application.lookup.equipment.MaintenanceProcedureOperationParameter import (
+    MaintenanceProcedureOperationParameter,
+)
 from src.application.lookup.equipment.Manufacturer import Manufacturer
 from src.application.lookup.equipment.Unit import Unit
-from src.port_adapter.api.grpc.listener.lookup.BaseLookupListener import BaseLookupListener
-from src.resource.proto._generated.lookup.equipment.building_level_pb2 import BuildingLevel as ProtoBuildingLevel
-from src.resource.proto._generated.lookup.equipment.building_level_room_pb2 import \
-    BuildingLevelRoom as ProtoBuildingLevelRoom
-from src.resource.proto._generated.lookup.equipment.building_pb2 import Building as ProtoBuilding
-from src.resource.proto._generated.lookup.equipment.equipment_category_group_pb2 import \
-    EquipmentCategoryGroup as ProtoEquipmentCategoryGroup
-from src.resource.proto._generated.lookup.equipment.equipment_lookup_app_service_pb2 import \
-    EquipmentLookupAppService_lookupResponse
-from src.resource.proto._generated.lookup.equipment.equipment_lookup_app_service_pb2_grpc import \
-    EquipmentLookupAppServiceServicer
-from src.resource.proto._generated.lookup.equipment.equipment_model_pb2 import EquipmentModel as ProtoEquipmentModel
-from src.resource.proto._generated.lookup.equipment.equipment_project_category_pb2 import \
-    EquipmentProjectCategory as ProtoEquipmentProjectCategory
-from src.resource.proto._generated.lookup.equipment.maintenance_procedure_operation_parameter_pb2 import \
-    MaintenanceProcedureOperationParameter as ProtoMaintenanceProcedureOperationParameter
-from src.resource.proto._generated.lookup.equipment.maintenance_procedure_operation_pb2 import \
-    MaintenanceProcedureOperation as ProtoMaintenanceProcedureOperation
-from src.resource.proto._generated.lookup.equipment.maintenance_procedure_pb2 import \
-    MaintenanceProcedure as ProtoMaintenanceProcedure
-from src.resource.proto._generated.lookup.equipment.manufacturer_pb2 import Manufacturer as ProtoManufacturer
+from src.port_adapter.api.grpc.listener.lookup.BaseLookupListener import (
+    BaseLookupListener,
+)
+from src.resource.proto._generated.lookup.equipment.building_level_pb2 import (
+    BuildingLevel as ProtoBuildingLevel,
+)
+from src.resource.proto._generated.lookup.equipment.building_level_room_pb2 import (
+    BuildingLevelRoom as ProtoBuildingLevelRoom,
+)
+from src.resource.proto._generated.lookup.equipment.building_pb2 import (
+    Building as ProtoBuilding,
+)
+from src.resource.proto._generated.lookup.equipment.equipment_category_group_pb2 import (
+    EquipmentCategoryGroup as ProtoEquipmentCategoryGroup,
+)
+from src.resource.proto._generated.lookup.equipment.equipment_lookup_app_service_pb2 import (
+    EquipmentLookupAppService_lookupResponse,
+)
+from src.resource.proto._generated.lookup.equipment.equipment_lookup_app_service_pb2_grpc import (
+    EquipmentLookupAppServiceServicer,
+)
+from src.resource.proto._generated.lookup.equipment.equipment_model_pb2 import (
+    EquipmentModel as ProtoEquipmentModel,
+)
+from src.resource.proto._generated.lookup.equipment.equipment_project_category_pb2 import (
+    EquipmentProjectCategory as ProtoEquipmentProjectCategory,
+)
+from src.resource.proto._generated.lookup.equipment.maintenance_procedure_operation_parameter_pb2 import (
+    MaintenanceProcedureOperationParameter as ProtoMaintenanceProcedureOperationParameter,
+)
+from src.resource.proto._generated.lookup.equipment.maintenance_procedure_operation_pb2 import (
+    MaintenanceProcedureOperation as ProtoMaintenanceProcedureOperation,
+)
+from src.resource.proto._generated.lookup.equipment.maintenance_procedure_pb2 import (
+    MaintenanceProcedure as ProtoMaintenanceProcedure,
+)
+from src.resource.proto._generated.lookup.equipment.manufacturer_pb2 import (
+    Manufacturer as ProtoManufacturer,
+)
 from src.resource.proto._generated.lookup.equipment.unit_pb2 import Unit as ProtoUnit
 
 
-class EquipmentLookupAppServiceListener(BaseLookupListener, EquipmentLookupAppServiceServicer):
+class EquipmentLookupAppServiceListener(
+    BaseLookupListener, EquipmentLookupAppServiceServicer
+):
     """The listener function implements the rpc call as described in the .proto file"""
 
     def __init__(self):
@@ -50,11 +79,10 @@ class EquipmentLookupAppServiceListener(BaseLookupListener, EquipmentLookupAppSe
             EquipmentLookupApplicationService
         )
         self._lookupResponse = EquipmentLookupAppService_lookupResponse
-        self._responseAttribute = 'equipments'
+        self._responseAttribute = "equipments"
 
     def __str__(self):
         return self.__class__.__name__
-
 
     def _lookupModelDataTypeToGrpcType(self, modelDataType):
         mapping = {
