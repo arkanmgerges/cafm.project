1cd4ffd6e001bf46c43e767633cbea60fd3224dd
bc <bara.catalin@gmail.com>
2021-06-25 12:18:57 +0300

    create for for user access

---
+++ src/port_adapter/messaging/listener/common/handler/user/RoleForUserAccessCreatedHandler.py
@@ -0,0 +1,43 @@
+"""
+@author: Arkan M. Gerges<arkan.m.gerges@gmail.com>
+"""
+import json
+
+from src.domain_model.event.EventConstant import CommonEventConstant
+from src.domain_model.resource.exception.UnAuthorizedException import (
+    UnAuthorizedException,
+)
+from src.port_adapter.messaging.listener.CommandConstant import CommonCommandConstant
+from src.port_adapter.messaging.listener.common.handler.Handler import Handler
+from src.resource.common.DateTimeHelper import DateTimeHelper
+from src.resource.logging.logger import logger
+
+
+class RoleForUserAccessCreatedHandler(Handler):
+    def __init__(self):
+        self._eventConstant = CommonEventConstant.ROLE_FOR_USER_ACCESS_CREATED
+        self._commandConstant = CommonCommandConstant.ASSIGN_ROLE_TO_USER
+
+    def canHandle(self, name: str) -> bool:
+        return name == self._eventConstant.value
+
+    def handleMessage(self, messageData: dict, extraData: dict = None) -> dict:
+        name = messageData["name"]
+        data = messageData["data"]
+        metadata = messageData["metadata"]
+
+        logger.debug(
+            f"[{RoleForUserAccessCreatedHandler.handleMessage.__qualname__}] - received args:\ntype(name): {type(name)}, name: {name}\ntype(data): {type(data)}, data: {data}\ntype(metadata): {type(metadata)}, metadata: {metadata}"
+        )
+        dataDict = json.loads(data)
+        metadataDict = json.loads(metadata)
+
+        if "token" not in metadataDict:
+            raise UnAuthorizedException()
+
+        return {
+            "name": self._commandConstant.value,
+            "created_on": DateTimeHelper.utcNow(),
+            "data": dataDict,
+            "metadata": metadataDict,
+        }
---
+++ src/port_adapter/messaging/listener/identity_event/handler/user/RoleForUserAccessCreatedHandler.py
@@ -0,0 +1,11 @@
+"""
+@author: Arkan M. Gerges<arkan.m.gerges@gmail.com>
+"""
+
+from src.port_adapter.messaging.listener.common.handler.user.RoleForUserAccessCreatedHandler import (
+    RoleForUserAccessCreatedHandler as Handler,
+)
+
+
+class RoleForUserAccessCreatedHandler(Handler):
+    pass
