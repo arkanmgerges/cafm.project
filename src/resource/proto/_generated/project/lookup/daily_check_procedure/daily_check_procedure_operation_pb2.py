fde0627b461e2ed68b597ba6f2ab49f5300e4a07
bc <bara.catalin@gmail.com>
2021-07-21 13:38:56 +0300

    add operation labels to daily procedures lookup

--- src/resource/proto/_generated/project/lookup/daily_check_procedure/daily_check_procedure_operation_pb2.py
+++ src/resource/proto/_generated/project/lookup/daily_check_procedure/daily_check_procedure_operation_pb2.py
@@ -12,6 +12,7 @@ _sym_db = _symbol_database.Default()
 
 
 from project.lookup.daily_check_procedure import daily_check_procedure_operation_parameter_pb2 as project_dot_lookup_dot_daily__check__procedure_dot_daily__check__procedure__operation__parameter__pb2
+from project.lookup.daily_check_procedure import daily_check_procedure_operation_label_pb2 as project_dot_lookup_dot_daily__check__procedure_dot_daily__check__procedure__operation__label__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
@@ -20,9 +21,9 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\nJproject/lookup/daily_check_procedure/daily_check_procedure_operation.proto\x12)cafm.project.lookup.daily_check_procedure\x1aTproject/lookup/daily_check_procedure/daily_check_procedure_operation_parameter.proto\"\xe2\x01\n\x1c\x44\x61ilyCheckProcedureOperation\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x0c\n\x04type\x18\x04 \x01(\t\x12\x84\x01\n*daily_check_procedure_operation_parameters\x18\x05 \x03(\x0b\x32P.cafm.project.lookup.daily_check_procedure.DailyCheckProcedureOperationParameterb\x06proto3'
+  serialized_pb=b'\nJproject/lookup/daily_check_procedure/daily_check_procedure_operation.proto\x12)cafm.project.lookup.daily_check_procedure\x1aTproject/lookup/daily_check_procedure/daily_check_procedure_operation_parameter.proto\x1aPproject/lookup/daily_check_procedure/daily_check_procedure_operation_label.proto\"\xe0\x02\n\x1c\x44\x61ilyCheckProcedureOperation\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x0c\n\x04type\x18\x04 \x01(\t\x12\x84\x01\n*daily_check_procedure_operation_parameters\x18\x05 \x03(\x0b\x32P.cafm.project.lookup.daily_check_procedure.DailyCheckProcedureOperationParameter\x12|\n&daily_check_procedure_operation_labels\x18\x06 \x03(\x0b\x32L.cafm.project.lookup.daily_check_procedure.DailyCheckProcedureOperationLabelb\x06proto3'
   ,
-  dependencies=[project_dot_lookup_dot_daily__check__procedure_dot_daily__check__procedure__operation__parameter__pb2.DESCRIPTOR,])
+  dependencies=[project_dot_lookup_dot_daily__check__procedure_dot_daily__check__procedure__operation__parameter__pb2.DESCRIPTOR,project_dot_lookup_dot_daily__check__procedure_dot_daily__check__procedure__operation__label__pb2.DESCRIPTOR,])
 
 
 
@@ -70,6 +71,13 @@ _DAILYCHECKPROCEDUREOPERATION = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='daily_check_procedure_operation_labels', full_name='cafm.project.lookup.daily_check_procedure.DailyCheckProcedureOperation.daily_check_procedure_operation_labels', index=5,
+      number=6, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -82,11 +90,12 @@ _DAILYCHECKPROCEDUREOPERATION = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=208,
-  serialized_end=434,
+  serialized_start=290,
+  serialized_end=642,
 )
 
 _DAILYCHECKPROCEDUREOPERATION.fields_by_name['daily_check_procedure_operation_parameters'].message_type = project_dot_lookup_dot_daily__check__procedure_dot_daily__check__procedure__operation__parameter__pb2._DAILYCHECKPROCEDUREOPERATIONPARAMETER
+_DAILYCHECKPROCEDUREOPERATION.fields_by_name['daily_check_procedure_operation_labels'].message_type = project_dot_lookup_dot_daily__check__procedure_dot_daily__check__procedure__operation__label__pb2._DAILYCHECKPROCEDUREOPERATIONLABEL
 DESCRIPTOR.message_types_by_name['DailyCheckProcedureOperation'] = _DAILYCHECKPROCEDUREOPERATION
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
