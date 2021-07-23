99629076c96f3b2cac150287b9c1d00a8c7625fc
Arkan M. Gerges <arkan.m.gerges@gmail.com>
2021-07-23 20:25:58 +0300

    Add verification to update by query validator

--- src/port_adapter/repository/lookup/common/es/UpdateByQueryValidator.py
+++ src/port_adapter/repository/lookup/common/es/UpdateByQueryValidator.py
@@ -2,9 +2,14 @@
 @author: Arkan M. Gerges<arkan.m.gerges@gmail.com>
 """
 
+
 class UpdateByQueryValidator:
     @classmethod
     def validate(cls, response):
         resDict = response.to_dict()
+        if 'updated' not in resDict:
+            from src.resource.logging.logger import logger
+            logger.error(f'Elastic error for update by query with result dict: {resDict}')
+            raise Exception(f"{resDict}")
         if resDict["updated"] == 0:
             raise Exception(f"{resDict}")
