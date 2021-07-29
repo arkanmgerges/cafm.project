9883aaea7364319c56f904f20b2bb0d17a76251a
bc <bara.catalin@gmail.com>
2021-07-29 09:35:31 +0300

    delete params or labels when changing maintenance op type

--- src/domain_model/project/maintenance/procedure/operation/label/MaintenanceProcedureOperationLabelRepository.py
+++ src/domain_model/project/maintenance/procedure/operation/label/MaintenanceProcedureOperationLabelRepository.py
@@ -83,3 +83,26 @@ class MaintenanceProcedureOperationLabelRepository(ABC):
         Returns:
             dict: A dict that has {"items": [], "totalItemCount": 0}
         """
+
+    @abstractmethod
+    def maintenanceProcedureOperationLabelsByMaintenanceProcedureOperationId(
+        self,
+        tokenData: TokenData,
+        maintenanceProcedureOperationId: str = None,
+        resultFrom: int = 0,
+        resultSize: int = 100,
+        order: List[dict] = None,
+    ) -> dict:
+        """Get list of maintenance procedure operation labels by maintenance_procedure_operation_id based on the owned roles that the user has
+
+        Args:
+            tokenData (TokenData): A token data object
+            MaintenanceProcedureOperationId: A maintenance_procedure_operation_id
+            resultFrom (int): The start offset of the result item
+            resultSize (int): The size of the items in the result
+            order (List[dict]): A list of order e.g. [{'orderBy': 'name', 'direction': 'asc'},
+                                {'orderBy': 'quantity', 'direction': 'desc'}]
+
+        Returns:
+            dict: A dict that has {"items": [], "totalItemCount": 0}
+        """
